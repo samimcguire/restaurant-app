796902aa80b28d87db5596686651d0ded61db6d1
Sami McGuire <samimcguire@gmail.com>
2024-02-10 16:41:40 -0700

    created pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+import { useContext, useState } from "react";
+import Head from "next/head";
+import AppContext from "@/components/context";
+import Layout from "@/components/layout";
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+function MyApp(props){
+  var {user, setUser} = useContext(AppContext)
+  const { Component, pageProps } = props
+
+  return (
+    <AppContext.Provider value={{isAuthenticated:false, user:null, setUser:()=>{}}}>
+      <Head />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+    </AppContext.Provider>
+  );
 }
+
+export default MyApp;
